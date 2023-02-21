3fe6902dc87a8ec6ac23218db6452df6842c8b49
Lethabo <lethaboradebe37@gmail.com>
2023-02-21 09:45:04 -0800

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,8 @@
 /**
  * _islower - Check for lowercase characters
  * @c: The character to be checked
- * Return 1 for lowercase character or 0 for anything else
+ * Return: 1 for lowercase character or 0 for anything else
  */
-
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
