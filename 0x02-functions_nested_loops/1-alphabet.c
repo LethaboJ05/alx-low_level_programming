a52f35e50df6f873efdf37e11d66cef467a8afd9
Lethabo <lethaboradebe37@gmail.com>
2023-02-21 08:33:04 -0800

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ * Return: 0 success
+ */
+void print_alphabet(void)
+{
+	int start = 97, end = 122;
+
+	while (start <= end)
+	{
+		_putchar(start);
+		start++;
+	}
+	_putchar('\n')
+}
